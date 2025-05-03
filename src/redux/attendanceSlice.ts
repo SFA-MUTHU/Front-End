407ec45597ea4e8470244aa99b15e4b95e08be51
DhananjayaDilz <dhananjaya.20221596@iit.ac.lk>
2025-05-03 13:54:17 +0530

    Create attendanceSlice.ts

---
+++ src/redux/attendanceSlice.ts
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { fetchAllAttendances,  Attendance } from '../services/employeeService';
+
+interface AttendanceState {
+  attendances: Attendance[];
+  employeeAttendances: Record<number, Attendance[]>;
+  loadingAttendance: boolean;
+  errorAttendance: string | null;
+}
+
+const initialState: AttendanceState = {
+  attendances: [],
+  employeeAttendances: {},
+  loadingAttendance: false,
+  errorAttendance: null,
+};
+
+export const getAllAttendances = createAsyncThunk(
+  'attendances/getAllAttendances',
+  async (_, { rejectWithValue }) => {
+    try {
+      return await fetchAllAttendances();
+    } catch (error) {
+      return rejectWithValue(error instanceof Error ? error.message : 'Failed to fetch attendances');
+    }
+  }
+);
+
+
+
+const attendanceSlice = createSlice({
+  name: 'attendances',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllAttendances.pending, (state) => {
+        state.loadingAttendance = true;
+        state.errorAttendance = null;
+      })
+      .addCase(getAllAttendances.fulfilled, (state, action) => {
+        state.loadingAttendance = false;
+        state.attendances = action.payload;
+      })
+      .addCase(getAllAttendances.rejected, (state, action) => {
+        state.loadingAttendance = false;
+        state.errorAttendance = action.payload as string;
+      })
+      
+  },
+});
+
+export default attendanceSlice.reducer;
